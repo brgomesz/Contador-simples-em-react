4ba12d969ff82d68eaecfda82ac3d7ecac8a92e2
Bruno Gomes <bruno.rodrigues.gomes@hotmail.com>
2025-02-06 21:05:08 -0300

    Meu primeiro contador em react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const increment10 = () => {
+    setCount(count + 10);
+  }
+  
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const decrement10 = () => {
+    setCount(count - 10)
+  }
+
+  const resset = () => {
+    setCount(count == 0)
+  }
+
+  // Aqui abaixo vai o que seria o HTML
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Contador simples</h1>
+        <div className="counter">
+          <button onClick={decrement10}> -10 </button>
+          <button onClick={decrement}>-1</button>
+          <span>{count}</span>
+          <button onClick={increment}>+1</button>
+          <button onClick={increment10}>+10</button>
+        </div>
+        <button onClick={resset}>Resset</button>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
+
